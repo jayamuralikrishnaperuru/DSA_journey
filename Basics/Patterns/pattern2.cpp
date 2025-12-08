518dd827d13df6d385cefd55f3044b20e86a76e2
jayamuralikrishnaperuru <muralikrishna3407@gmail.com>
2025-12-08 21:36:11 +0530

    added the 2nd file

---
+++ Basics/Patterns/pattern2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    void pattern2(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                cout << "*" ;
+            }
+            cout << endl;
+        }
+    }
+};
+int main(){
+    int N = 5;
+    Solution sol;
+    sol.pattern2(N);
+    return 0;
+}
