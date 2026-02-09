d8d16b80a3143bfdfefc51c0044519624f43c1aa
PERURU Jaya Murali Krishna <muralikrishna3407@gmail.com>
2026-02-09 14:24:44 +0530

    Finding the subarray with the largest sum and returns it number

---
+++ Basics/Basic Maths/MaxSubArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int maxSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            maxSum = max(maxSum, sum);
+        }
+    }
+
+    cout << maxSum << endl;
+
+    return 0;
+}
