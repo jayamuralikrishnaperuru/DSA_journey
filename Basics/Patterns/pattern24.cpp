f514406497515a4ebead8cffaaa26568e7a98312
PERURU Jaya Murali Krishna <muralikrishna3407@gmail.com>
2026-02-13 11:16:18 +0530

    Sum of princple diagonal elements of a 5x5 matrix

--- Basics/Patterns/pattern24.cpp
+++ Basics/Patterns/pattern24.cpp
@@ -1,19 +1,41 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a[5][5] = {
+//         {1, 44, 33, 22, 11},
+//         {44, 1, 22, 11, 88},
+//         {33, 22, 1, 77, 88},
+//         {11, 100, 99, 1, 77},
+//         {100, 99, 88, 77, 1}};
+//     int sum = 0;
+//     for (int i = 0; i < 5; i++)
+//     {
+//         sum += a[i][i];
+//     }
+//     cout << sum << endl;
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int a[5][5] = {
-        {1, 44, 33, 22, 11},
-        {44, 1, 22, 11, 88},
-        {33, 22, 1, 77, 88},
-        {11, 100, 99, 1, 77},
-        {100, 99, 88, 77, 1}};
+    int a[5][5];
     int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
     for (int i = 0; i < 5; i++)
     {
         sum += a[i][i];
     }
-    cout << sum << endl;
+
+    cout << "Sum of diagonal elements is : " << sum << endl;
     return 0;
 }
